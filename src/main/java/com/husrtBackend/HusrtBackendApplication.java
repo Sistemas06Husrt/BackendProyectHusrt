5687f44872a630455a7c9d269dcfeac5a0d5b7a7
davidguerrero3141 <sistemas6@hospitalsanrafaeltunja.gov.co>
2024-08-22 09:37:47 -0500

    first commit

---
+++ src/main/java/com/husrtBackend/HusrtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.husrtBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HusrtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HusrtBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/husrtBackend/HusrtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.husrtBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HusrtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
